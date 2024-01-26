532ea30ec655775eda083781f6094e6004327cf9
TOTON Lionel <totonlionel@gmail.com>
2024-01-26 19:07:05 +0100

    le commit pour le cours de React

---
+++ 04tut/src/Header.js
@@ -0,0 +1,15 @@
+
+// import './App.css';
+
+
+const Header = () => {
+   
+    
+    return (
+        <header>
+            <h1>Groceries List</h1>
+      </header>
+        
+    )
+}
+export default Header;
