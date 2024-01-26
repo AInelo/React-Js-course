e47bed2d5c095030d55655b907f3b66fb3d79183
TOTON Lionel <totonlionel@gmail.com>
2024-01-26 10:08:06 +0100

    le commit pour le cours de React

--- 02tut/src/App.js
+++ 02tut/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Lionel";
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and saveee to reload.
+          Edit <code>src/App.js</code> and save to see what I change.
         </p>
         <a
           className="App-link"
