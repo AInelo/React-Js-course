4aaf08ebf790ead22500c3417901a55ecd34690b
TOTON Lionel <totonlionel@gmail.com>
2024-01-26 18:21:38 +0100

    le commit pour le cours de React

--- 03tut/src/App.js
+++ 03tut/src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
 
 function App() {
-  const name = "Lionel";
-  const handleNameChange = () => {
-    const names = ['Bob', 'Axel', 'Lionel'];
-    const int = Math.floor(Math.random() * 3);
-    return names[int]
-  }
+  
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World {handleNameChange()}!
-        </p>
-        
-      </header>
-      
+      <Header/>
     </div>
   );
 }
---
+++ 03tut/src/Header.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+const Header = () => {
+    const name = "Lionel";
+  const handleNameChange = () => {
+    const names = ['Bob', 'Axel', 'Lionel'];
+    const int = Math.floor(Math.random() * 3);
+    return names[int]
+  }
+
+    return (
+        <header className="App-header">
+            <h1>Groceries List</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World {handleNameChange()}!
+        </p>
+        
+      </header>
+        
+    )
+}
+
+export default Header;
