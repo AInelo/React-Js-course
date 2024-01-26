2ccd70cba73d3cef27e814f3737c08b75352de76
TOTON Lionel <totonlionel@gmail.com>
2024-01-26 20:57:01 +0100

    le commit pour le cours de React

---
+++ 06tut/README.md
@@ -0,0 +1,3 @@
+# useState Hook
+
+1. How to Add css function in React with JSX
---
+++ 06tut/src/Header.js
@@ -0,0 +1,17 @@
+
+const Header = () => {
+    const hearderStyle = {
+        backgroundColor: 'royalblue', 
+        color: '#fff'
+
+    };
+   
+    
+    return (
+        <header style={hearderStyle} >
+            <h1>Groceries List</h1>
+      </header>
+        
+    )
+}
+export default Header;
