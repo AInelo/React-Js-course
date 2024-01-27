1a54dab1a415b5574eb76d12f8fd6904b49e45d7
TOTON Lionel <totonlionel@gmail.com>
2024-01-27 15:52:11 +0100

    le commit pour le cours de React

--- 08tut/src/Content.js
+++ 08tut/src/Content.js
@@ -1,47 +1,9 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { FaTrashAlt } from 'react-icons/fa'
 
 
-const Content = () => {
-/*
-  This is useState usage part
-*/
-  const [items, setItems ] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half pound bag of Cocoa Covered"
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2 Lionel",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3 For Lionel",
-    }
-  ]);
-
-  const handleCheck = (id) => {
-    console.log(`key: ${id}`);
-    // const listItems = items.map
-    // const listItems = [...items]
-    const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-  }
-
-
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-  }
-  
-
-    return (
+const Content = ({items, handleCheck, handleDelete }) => {
+ return (
         <main>
           {items.length ? (
               <ul>
