b96c94903f034cdcf70193180d64b6846b03d3d9
TOTON Lionel <totonlionel@gmail.com>
2024-01-27 10:39:56 +0100

    le commit pour le cours de React

---
+++ 06tut (copy 1)/src/Content.js
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+
+
+const Content = () => {
+/*
+  This is useState usage part
+*/
+  const [name, setName ] = useState('Lionel');
+  const [count, setCount] = useState(0);
+
+  const handleNameChange = () => {
+    const names = ['Bobe', 'Axel', 'Lionel'];
+    const int = Math.floor(Math.random() * 3);
+    setName(names[int]) 
+    return names[int]
+  }
+
+  const handleClick = () => {
+    setCount(count + 1);
+    setCount(count + 1);    
+    console.log(count)
+  }
+
+  const handleClick2 = (name) => {
+    console.log(count)
+  }
+
+  // const handleClick3 = (e) => {
+  //   console.log(e.target.innerText)
+  // }
+
+
+    return (
+        <main>
+            <p onDoubleClick={handleClick}>
+              Hello World {name}!
+            </p>
+            <button onClick={handleNameChange} >
+              Change name
+            </button>
+
+            <button onClick={handleClick} >
+              Click it
+            </button>
+
+            <button onClick={handleClick2} >
+              Click it
+            </button>
+        </main>
+    )
+}
+
+export default Content;
