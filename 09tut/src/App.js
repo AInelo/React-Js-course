1bc358fa8e7324dbb2157bbdf43806b7daa192d6
TOTON Lionel <totonlionel@gmail.com>
2024-01-28 12:21:53 +0100

    le commit pour le cours de React

--- 09tut/src/App.js
+++ 09tut/src/App.js
@@ -7,47 +7,43 @@ import AddItem from './AddItems';
 
 
 function App() {
-  const [items, setItems ] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half pound bag of Cocoa Covered"
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2 Lionel",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3 For Lionel",
-    }
-  ]);
+  const [items, setItems ] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
 
-  const [newItem, setNewItem] = useState('')
+  const [newItem, setNewItem] = useState('');
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+  }
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked : false, item };
+    const listItems = [...items, myNewItem];
+    setAndSaveItems(listItems)
+  }
 
   const handleCheck = (id) => {
     console.log(`key: ${id}`);
     // const listItems = items.map
     // const listItems = [...items]
     const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    setAndSaveItems(listItems)
   }
 
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    setAndSaveItems(listItems)
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!newItem) return;
+    console.log(newItem)
     // addItem function
-    console.log('submitted')
+    addItem(newItem);
+    console.log('');
   }
 
 
