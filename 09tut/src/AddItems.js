3fd209f2621f10728a974b848ccbb9871ec89b58
TOTON Lionel <totonlionel@gmail.com>
2024-01-29 07:57:19 +0100

    le commit pour le cours de React

--- 09tut/src/AddItems.js
+++ 09tut/src/AddItems.js
@@ -5,24 +5,24 @@ const AddItem = ({newItem, setNewItem, handleSubmit}) => {
     const inputRef = useRef();
 
     return (
-        <form className="addForm" onSubmit={handleSubmit} >
-            <label htmlFor="addItem">Add Item</label>
-            <input 
+        <form className='addForm' onSubmit={handleSubmit}>
+            <label htmlFor='addItem'>Add Item</label>
+            <input
                 autoFocus
                 ref={inputRef}
-                id="addItem"
-                placeholder="Add Item"
-                type="text"
+                id='addItem'
+                type='text'
+                placeholder='Add Item'
                 required
                 value={newItem}
                 onChange={(e) => setNewItem(e.target.value)}
             />
             <button
-                type="submit"
-                aria-label="Add Item"
+                type='submit'
+                aria-label='Add Item'
                 onClick={() => inputRef.current.focus()}
             >
-                <FaPlus/>
+                <FaPlus />
             </button>
         </form>
     )
