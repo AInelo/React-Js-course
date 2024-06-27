a658c48b2cc7760e51ca2d210aae25d2b9e3d5b7
TOTON Lionel <totonlionel@gmail.com>
2024-06-27 11:57:14 +0100

    le commit pour le cours de React

---
+++ 10tut ProjectChallenge/README.md
@@ -0,0 +1,3 @@
+# Challenge Apps
+
+1. 
--- 10tut ProjectChallenge/src/index.js
+++ 10tut ProjectChallenge/src/index.js
@@ -10,40 +10,6 @@ root.render(
   </React.StrictMode>
 );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-
-
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
 // ReactDOM.render(
 //   <React.StrictMode>
 //     <App />
